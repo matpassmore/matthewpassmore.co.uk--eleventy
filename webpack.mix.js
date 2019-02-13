0494d69016789ffaa9c4031b9bc03b7d65127d1c
Matthew Passmore <matthew@matthewpassmore.co.uk>
2019-02-13 09:44:30 +0000

    feat (performance): remove unused css classes in production build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,17 @@
 let mix = require('laravel-mix');
 var tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 /*
  |--------------------------------------------------------------------------
@@ -38,6 +50,30 @@ mix
       './**/*.css',
     ]
   });
+
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new PurgecssPlugin({
+
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, "src/site/**/*.njk"),
+          path.join(__dirname, "src/js/**/*.js")
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ["html", "js", "njk"]
+          }
+        ]
+      })
+    ]
+  });
+}
 
 // Full API
 // mix.js(src, output);
